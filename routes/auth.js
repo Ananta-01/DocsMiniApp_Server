e24e9f47ef1f0d327878fd330e6c163b0d462e29
Ananta-01 <anantasontakke143@gmail.com>
2024-01-27 13:29:31 +0530

    register and login Done

--- routes/auth.js
+++ routes/auth.js
@@ -1,12 +1,35 @@
 const router = require("express").Router();
 const User = require("../models/User");
 
-router.get("/login", (req,res)=>{
-    res.send("User Logged In");
+router.post("/login", async (req, res) => {
+  try {
+    const user = await User.findOne({ email: req.body.email });
+
+    !user && res.status(404).json({ message: "User not found", status: false });
+    const validatePassword = req.body.password == user.password;
+    !validatePassword &&
+      res.status(400).json({ message: "wrong password", status: false });
+
+    res.status(200).json({ ...user, message: "user found", status: true });
+  } catch (error) {
+    res.status(500).json(error);
+  }
 });
 
-router.get("/reqister", (req,res)=>{
-    res.send("User registered");
-} );
+router.post("/reqister", async (req, res) => {
+  try {
+    const user = new User({
+      name: req.body.name,
+      email: req.body.email,
+      password: req.body.password,
+    });
 
-module.exports = router
+    const data = await user.save();
+
+    res.status(200).json(data);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+module.exports = router;
