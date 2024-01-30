b862f68e68174b17c1bacec6f75833c0ff17c33e
Ananta-01 <anantasontakke143@gmail.com>
2024-01-31 00:29:27 +0530

    dev

--- index.js
+++ index.js
@@ -33,6 +33,8 @@ app.use("/api/auth", authRouter)
 app.use("/api/notes", notesRouter)
 
 // Start the server
-app.listen(8000, () => {
-  console.log("App is running on port 8000");
+const PORT = process.env.PORT || 8000;
+
+app.listen(PORT, () => {
+  console.log(`App is running on port ${PORT}`);
 });
